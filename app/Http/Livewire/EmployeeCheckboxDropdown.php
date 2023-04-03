80761ba0086e47f3d5733dd2433e56bf3842c76d
mis-kcn <kcniverba@myinternetsupport.com>
2023-04-04 07:49:46 +0800

    feat: create group and department

---
+++ app/Http/Livewire/EmployeeCheckboxDropdown.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EmployeeCheckboxDropdown extends Component
+{
+    public function render()
+    {
+        return view('livewire.employee-checkbox-dropdown');
+    }
+}
