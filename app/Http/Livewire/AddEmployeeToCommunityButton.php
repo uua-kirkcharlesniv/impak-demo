63adf3073b77d80c171a32f02837e1e58b1abf76
mis-kcn <kcniverba@myinternetsupport.com>
2023-04-04 08:17:31 +0800

    feat: add member to community

---
+++ app/Http/Livewire/AddEmployeeToCommunityButton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AddEmployeeToCommunityButton extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-employee-to-community-button');
+    }
+}
