7778a4eb196fa9f3be0daccc6ca18d553206a13e
mis-kcn <kcniverba@myinternetsupport.com>
2023-08-04 05:37:34 +0800

    fix: remove mistyped name

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Survey;
-use App\Models\Templaste;
 use App\Models\Template;
 use Exception;
 use Illuminate\Support\Facades\Auth;
