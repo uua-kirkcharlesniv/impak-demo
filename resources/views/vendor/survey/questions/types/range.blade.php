1e0f056c89ae161b73843ed6de2efde73d429299
Kirk Charles Niverba <kirk@moveup.app>
2023-08-07 07:40:59 +0800

    fix: range slider not working

--- resources/views/vendor/survey/questions/types/range.blade.php
+++ resources/views/vendor/survey/questions/types/range.blade.php
@@ -1,4 +1,16 @@
 @component('survey::questions.base', compact('question'))
-    <input type="range" min="0" max="10" step="1" name="{{ $question->key }}" id="{{ $question->key }}"
-        class="w-full">
+    <input type="range" min="1" max="10" step="1" name="{{ $question->key }}" id="{{ $question->key }}"
+        class="w-full" list="markers">
+    <datalist id="markers">
+        <option value="1"></option>
+        <option value="2"></option>
+        <option value="3"></option>
+        <option value="4"></option>
+        <option value="5"></option>
+        <option value="6"></option>
+        <option value="7"></option>
+        <option value="8"></option>
+        <option value="9"></option>
+        <option value="10"></option>
+    </datalist>
 @endcomponent
