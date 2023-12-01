2128f207966fa6c4bc6abda6380d8ad099291469
Kirk Charles Niverba <kirk@moveup.app>
2023-12-01 13:50:02 +0800

    feat: impak email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+<img src="file://{{ asset('images/logobrand.png') }}" class="logo" alt="Impak Logo" />
 </a>
 </td>
 </tr>
