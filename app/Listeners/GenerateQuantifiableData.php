bdc8f5198d4a7cc4ac7bed42ea37ce1350a9d353
mis-kcn <kcniverba@myinternetsupport.com>
2023-12-06 15:09:38 +0800

    feat: generate quantifiable data

--- app/Listeners/GenerateQuantifiableData.php
+++ app/Listeners/GenerateQuantifiableData.php
@@ -3,8 +3,11 @@
 namespace App\Listeners;
 
 use App\Events\SurveyPublished;
+use App\Models\QuestionQuant;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+use MattDaneshvar\Survey\Models\Answer;
 
 class GenerateQuantifiableData
 {
@@ -31,9 +34,106 @@ class GenerateQuantifiableData
             return false;
         }
 
-        info('Starting generating quantifiable data...' . $survey->publish_status);
+        foreach ($survey->questions as $question) {
+            $questionId = $question->id;
+            
+            $data = Answer::select('value')->selectRaw('COUNT(*) as count')->where('question_id', $questionId)->groupBy('value')->get();
+            $dataset = $data->pluck('count', 'value')->toArray();
 
+            $labels = [];
+            $compiled = [];
 
-        info('Generating quantifiable data...');
+            $mean = -1;
+            $median = -1;
+            $mode = -1;
+            $stddev = 0;
+
+            if($question->type == 'range') {
+                foreach (range(1, 10) as $option) {
+                    array_push($labels, $option);
+    
+                    if (array_key_exists($option, $dataset)) {
+                        array_push($compiled, $dataset[$option]);
+                    } else {
+                        array_push($compiled, 0);
+                    }
+                }
+
+                $dataset = array_combine($labels, $compiled);
+                $data = $compiled;
+
+                // Mean 
+                $mean = array_sum($data) / count($data);
+                $values = array_values($data);
+                $count = count($values);
+                $middle = floor(($count - 1) / 2);
+
+                if ($count % 2 == 0) {
+                    $median = ($values[$middle] + $values[$middle + 1]) / 2;
+                } else {
+                    $median = $values[$middle];
+                }
+
+                // Step 4: Calculate the mode
+                $counts = array_count_values($values);
+                arsort($counts);
+                $mode = key($counts);
+
+                $variance = 0;
+                foreach ($data as $value) {
+                    $variance += pow($value - $mean, 2);
+                }
+                $stddev = sqrt($variance / count($data));
+
+                $data = $dataset;
+            } elseif($question->type == 'radio') {
+                $order = $question->options;
+                $dataset = array_reverse(array_merge(array_flip($order), $dataset));
+                $data = $dataset;
+
+                // Mean
+                $mean = array_sum($data) / count($data);
+
+                // Median
+                $count = count($data);
+                $middle = floor(($count - 1) / 2);
+                $keys = array_keys($data);
+
+                if ($count % 2 == 0) {
+                    $median = ($data[$keys[$middle]] + $data[$keys[$middle + 1]]) / 2;
+                } else {
+                    $median = $data[$keys[$middle]];
+                }
+                
+                // Mode
+                $counts = array_count_values($data);
+                arsort($counts);
+                $mode = key($counts);
+
+                $variance = 0;
+                foreach ($data as $value) {
+                    $variance += pow($value - $mean, 2);
+                }
+                $stddev = sqrt($variance / count($data));
+            }
+
+            Log::info(json_encode([
+                    'type' => $question->type,
+                    'mean' => $mean,
+                    'median' => $median,
+                    'mode' => $mode,
+                    'stddev' => $stddev,
+                    'data' => $data,
+                ]));
+
+            QuestionQuant::updateOrCreate([
+                'question_id' => $questionId
+            ], [
+                'mean' => $mean,
+                'median' => $median,
+                'mode' => $mode,
+                'dataset' => $data, 
+            ]);
+        }
     }
 }
--- app/Listeners/GenerateQuestionInsight.php
+++ app/Listeners/GenerateQuestionInsight.php
@@ -3,8 +3,11 @@
 namespace App\Listeners;
 
 use App\Events\SurveyPublished;
+use App\Models\QuestionQuant;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+use MattDaneshvar\Survey\Models\Answer;
 
 class GenerateQuestionInsight
 {
@@ -27,7 +30,22 @@ class GenerateQuestionInsight
     public function handle(SurveyPublished $event)
     {
         $survey = $event->survey;
+        if($survey->publish_status != "closed") {
+            return false;
+        }
 
-        info('Generating insights');
+        foreach ($survey->questions as $question) {
+            $questionId = $question->id;
+            $quant = QuestionQuant::where('question_id', $questionId)->get();
+
+            if(!$quant) {
+                return false;
+            } 
+
+            $data = Answer::select('value')->selectRaw('COUNT(*) as count')->where('question_id', $questionId)->groupBy('value')->get();
+            $dataset = $data->pluck('count', 'value')->toArray();
+
+            
+        }        
     }
 }
