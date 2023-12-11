594ba493d60a3d7174ccc9e5cf3f8b2bada9fa34
Kirk Charles Niverba <kirk@moveup.app>
2023-12-11 17:16:10 +0800

    fix: emplyoee sidebar caret orientation

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -125,7 +125,7 @@
                                 <!-- Icon -->
                                 <div
                                     class="flex shrink-0 ml-2 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                    <svg class="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 @if (in_array(Request::segment(1), ['dashboard'])) {{ 'rotate-180' }} @endif"
+                                    <svg class="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 @if (in_array(Request::segment(1), ['employee'])) {{ 'rotate-180' }} @endif"
                                         :class="open ? 'rotate-180' : 'rotate-0'" viewBox="0 0 12 12">
                                         <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
                                     </svg>
