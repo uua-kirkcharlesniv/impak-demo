0510a0f50d2d8886767e47d3b17961a0097f3ef9
Kirk Charles Niverba <kirk@moveup.app>
2024-02-16 11:06:07 +0800

    fix: add toggle password button

--- resources/views/components/settings/account-panel.blade.php
+++ resources/views/components/settings/account-panel.blade.php
@@ -16,30 +16,28 @@
                 <div class="flex flex-wrap mt-5">
                     <div class="mr-2">
                         <label class="sr-only" for="email">Business email</label>
-                        <input id="email" name="email" class="form-input" type="email"
-                            value="{{ Auth::user()->email }}" />
+                        <input id="email" name="email" class="form-input" type="email" value="{{ Auth::user()->email }}" />
                     </div>
-                    <button
-                        class="btn border-slate-200 hover:border-slate-300 shadow-sm text-indigo-500">Change</button>
+                    <button class="btn border-slate-200 hover:border-slate-300 shadow-sm text-indigo-500">Change</button>
                 </div>
                 @if ($errors->updateEmail->any())
-                    <div class="mt-4">
-                        <div class="px-4 py-2 rounded-sm text-sm bg-rose-100 border border-rose-200 text-rose-600">
-                            <div class="font-medium">{{ __('Whoops! Something went wrong.') }}</div>
-                            <ul class="mt-1 list-disc list-inside text-sm">
-                                @foreach ($errors->updateEmail->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                <div class="mt-4">
+                    <div class="px-4 py-2 rounded-sm text-sm bg-rose-100 border border-rose-200 text-rose-600">
+                        <div class="font-medium">{{ __('Whoops! Something went wrong.') }}</div>
+                        <ul class="mt-1 list-disc list-inside text-sm">
+                            @foreach ($errors->updateEmail->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
+                </div>
                 @endif
                 @if (session()->has('updateEmailMessage'))
-                    <div class="mt-4">
-                        <div class="px-4 py-2 rounded-sm text-sm bg-green-100 border border-green-200 text-green-600">
-                            <div class="font-medium">{{ session()->get('updateEmailMessage') }}</div>
-                        </div>
+                <div class="mt-4">
+                    <div class="px-4 py-2 rounded-sm text-sm bg-green-100 border border-green-200 text-green-600">
+                        <div class="font-medium">{{ session()->get('updateEmailMessage') }}</div>
                     </div>
+                </div>
                 @endif
             </form>
         </section>
@@ -53,36 +51,36 @@
                 <div class="mt-5">
                     <div class="w-1/3">
                         <x-jet-label for="password" value="{{ __('Current Password') }}" />
-                        <x-jet-input id="old_password" type="password" name="old_password" required />
+                        <x-password-input id="old_password" type="password" name="old_password" required />
                     </div>
                     <div class="w-1/3 mt-2">
                         <x-jet-label for="password" value="{{ __('Password') }}" />
-                        <x-jet-input id="new_password" type="password" name="new_password" required />
+                        <x-password-input id="new_password" type="password" name="new_password" required />
                     </div>
                     <div class="w-1/3 mt-2">
                         <x-jet-label for="password" value="{{ __('Confirm Password') }}" />
-                        <x-jet-input id="confirm_password" type="password" name="confirm_password" required />
+                        <x-password-input id="confirm_password" type="password" name="confirm_password" required />
                     </div>
                     <button class="btn border-slate-200 shadow-sm mt-4 text-indigo-500">Set New Password</button>
                 </div>
                 @if ($errors->updatePassword->any())
-                    <div class="mt-4">
-                        <div class="px-4 py-2 rounded-sm text-sm bg-rose-100 border border-rose-200 text-rose-600">
-                            <div class="font-medium">{{ __('Whoops! Something went wrong.') }}</div>
-                            <ul class="mt-1 list-disc list-inside text-sm">
-                                @foreach ($errors->updatePassword->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                <div class="mt-4">
+                    <div class="px-4 py-2 rounded-sm text-sm bg-rose-100 border border-rose-200 text-rose-600">
+                        <div class="font-medium">{{ __('Whoops! Something went wrong.') }}</div>
+                        <ul class="mt-1 list-disc list-inside text-sm">
+                            @foreach ($errors->updatePassword->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
+                </div>
                 @endif
                 @if (session()->has('updatePasswordMessage'))
-                    <div class="mt-4">
-                        <div class="px-4 py-2 rounded-sm text-sm bg-green-100 border border-green-200 text-green-600">
-                            <div class="font-medium">{{ session()->get('updatePasswordMessage') }}</div>
-                        </div>
+                <div class="mt-4">
+                    <div class="px-4 py-2 rounded-sm text-sm bg-green-100 border border-green-200 text-green-600">
+                        <div class="font-medium">{{ session()->get('updatePasswordMessage') }}</div>
                     </div>
+                </div>
                 @endif
             </form>
         </section>
