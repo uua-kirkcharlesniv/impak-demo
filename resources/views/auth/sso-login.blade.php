46e9b811367ed892f6afc3480f4e912301528771
Kirk Charles Niverba <kirk@moveup.app>
2024-02-16 10:46:20 +0800

    feat: add password input with toggle

--- resources/views/auth/sso-login.blade.php
+++ resources/views/auth/sso-login.blade.php
@@ -1,9 +1,9 @@
 <x-authentication-layout>
     <h1 class="text-3xl text-slate-800 font-bold mb-6">{{ __('Welcome back!') }} ✨</h1>
     @if (session('status'))
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ session('status') }}
-        </div>
+    <div class="mb-4 font-medium text-sm text-green-600">
+        {{ session('status') }}
+    </div>
     @endif
     <!-- Form -->
     <form method="POST" action="{{ route('processCentralLogin') }}">
@@ -16,16 +16,16 @@
 
             <div>
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" type="password" name="password" required autocomplete="password" />
+                <x-password-input />
             </div>
         </div>
         <div class="flex items-center justify-between mt-6">
             @if (Route::has('password.request'))
-                <div class="mr-1">
-                    <a class="text-sm underline hover:no-underline" href="{{ route('password.request') }}">
-                        {{ __('Forgot Password?') }}
-                    </a>
-                </div>
+            <div class="mr-1">
+                <a class="text-sm underline hover:no-underline" href="{{ route('password.request') }}">
+                    {{ __('Forgot Password?') }}
+                </a>
+            </div>
             @endif
             <x-jet-button class="ml-3">
                 {{ __('Sign in') }}
@@ -36,8 +36,7 @@
     <!-- Footer -->
     <div class="pt-5 mt-6 border-t border-slate-200">
         <div class="text-sm">
-            {{ __('Don\'t you have an account?') }} <a class="font-medium text-indigo-500 hover:text-indigo-600"
-                href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+            {{ __('Don\'t you have an account?') }} <a class="font-medium text-indigo-500 hover:text-indigo-600" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
         </div>
         <!-- Warning -->
         {{-- <div class="mt-5">
