3b6420af1d90db974535b1db4469bfbec9c9b8f4
Kirk Charles Niverba <kirk@moveup.app>
2024-02-16 13:28:11 +0800

    fix: hide filter and search on campaigns

--- resources/views/pages/campaigns.blade.php
+++ resources/views/pages/campaigns.blade.php
@@ -13,10 +13,10 @@
             <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
 
                 <!-- Search form -->
-                <x-search-form />
+                {{-- <x-search-form /> --}}
 
                 <!-- Filter button -->
-                <x-dropdown-filter align="right" />
+                {{-- <x-dropdown-filter align="right" /> --}}
 
                 <!-- Create campaign button -->
                 <livewire:create-survey-button />
@@ -25,43 +25,41 @@
 
         </div>
 
-        @if ($campaigns->count() < 1)
-            <div class="max-w-2xl m-auto mt-16">
-                <div class="text-center px-4">
-                    <div
-                        class="inline-flex items-center justify-center w-16 h-16 rounded-full bg-gradient-to-t from-slate-200 to-slate-100 mb-4">
-                        👩🏻‍💻
-                    </div>
-                    <h2 class="text-2xl text-slate-800 font-bold mb-2">
-                        Embark on insights, as your survey journey starts here.
-                    </h2>
-                    <div class="mb-6">
-                        Begin your voyage into valuable insights with us. Explore a world of possibilities as you take
-                        your first steps in the realm of surveys, where every question leads to discovery.
-
-                    </div>
-
-                    <livewire:create-survey-button />
+        @if ($campaigns->count() < 1) <div class="max-w-2xl m-auto mt-16">
+            <div class="text-center px-4">
+                <div class="inline-flex items-center justify-center w-16 h-16 rounded-full bg-gradient-to-t from-slate-200 to-slate-100 mb-4">
+                    👩🏻‍💻
                 </div>
+                <h2 class="text-2xl text-slate-800 font-bold mb-2">
+                    Embark on insights, as your survey journey starts here.
+                </h2>
+                <div class="mb-6">
+                    Begin your voyage into valuable insights with us. Explore a world of possibilities as you take
+                    your first steps in the realm of surveys, where every question leads to discovery.
+
+                </div>
+
+                <livewire:create-survey-button />
             </div>
-        @endif
+    </div>
+    @endif
 
-        <!-- Cards -->
-        <div class="grid grid-cols-12 gap-6">
+    <!-- Cards -->
+    <div class="grid grid-cols-12 gap-6">
 
-            <!-- Campaign cards -->
-            @foreach ($campaigns as $campaign)
-                <x-campaigns.campaigns-card :campaign="$campaign" />
-            @endforeach
+        <!-- Campaign cards -->
+        @foreach ($campaigns as $campaign)
+        <x-campaigns.campaigns-card :campaign="$campaign" />
+        @endforeach
 
-        </div>
+    </div>
 
-        <!-- Pagination -->
-        @if (isset($campaigns->links))
-            <div class="mt-8">
-                {{ $campaigns->links() }}
-            </div>
-        @endif
+    <!-- Pagination -->
+    @if (isset($campaigns->links))
+    <div class="mt-8">
+        {{ $campaigns->links() }}
+    </div>
+    @endif
 
     </div>
 </x-app-layout>
