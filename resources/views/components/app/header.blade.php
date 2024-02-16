a85d5312e7a7558e49155f6d0f19e1e2112d7e54
Kirk Charles Niverba <kirk@moveup.app>
2024-02-16 11:02:42 +0800

    fix: remove non-functional search and info button

--- resources/views/components/app/header.blade.php
+++ resources/views/components/app/header.blade.php
@@ -4,14 +4,9 @@
 
             <!-- Header: Left side -->
             <div class="flex">
-                
+
                 <!-- Hamburger button -->
-                <button
-                    class="text-slate-500 hover:text-slate-600 lg:hidden"
-                    @click.stop="sidebarOpen = !sidebarOpen"
-                    aria-controls="sidebar"
-                    :aria-expanded="sidebarOpen"
-                >
+                <button class="text-slate-500 hover:text-slate-600 lg:hidden" @click.stop="sidebarOpen = !sidebarOpen" aria-controls="sidebar" :aria-expanded="sidebarOpen">
                     <span class="sr-only">Open sidebar</span>
                     <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <rect x="4" y="5" width="16" height="2" />
@@ -26,13 +21,13 @@
             <div class="flex items-center space-x-3">
 
                 <!-- Search Button with Modal -->
-                <x-modal-search />
+                {{-- <x-modal-search /> --}}
 
                 <!-- Notifications button -->
                 <x-dropdown-notifications align="right" />
 
                 <!-- Info button -->
-                <x-dropdown-help align="right" />
+                {{-- <x-dropdown-help align="right" /> --}}
 
                 <!-- Divider -->
                 <hr class="w-px h-6 bg-slate-200" />
